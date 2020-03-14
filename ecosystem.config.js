1d82630db88947f7f0511c1a439111e2f871abf0
冰风 <260895778@qq.com>
2020-03-14 20:35:18 +0800

    pm2的配置文件和忽略pm2文件的日志

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'index.js',
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '500M',
+    ignore_watch: [
+        "node_modules",
+        "uploads",
+        "logs",
+	".git",
+	"static"
+	
+    ],
+    error_file: "./logs/app-err.log",
+    out_file: "./logs/app-out.log"
+    }]
+};
